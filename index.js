eaa87a617d2cb0eb4b4e2ca965a15e72a34f7c1c
Ramesh Kuruba <ramesh.kuruba@ge.com>
2019-06-13 14:22:41 -0400

    API key removal

--- index.js
+++ index.js
@@ -3,14 +3,6 @@ const AWS = require('aws-sdk');
 
 exports.handler=async function(event, context, callback){
 
-  AWS.config.update({
-      // accessKeyId default can be used while using the downloadable version of DynamoDB. 
-      // For security reasons, do not store AWS Credentials in your files. Use Amazon Cognito instead.
-      accessKeyId: "AKIA44AK7D2TU5ZZQIOD",
-      // secretAccessKey default can be used while using the downloadable version of DynamoDB. 
-      // For security reasons, do not store AWS Credentials in your files. Use Amazon Cognito instead.
-      secretAccessKey: "77HVwgBC3batMQa0ksTBK+zvrJ8eR7+6n2H0fjlp"
-  });
   
 const res = await request.get('https://www.omdbapi.com/?t=Gladiator&y=&plot=short&apikey=trilogy', { statusCode: 200,
  body: "OMDB Data response",
